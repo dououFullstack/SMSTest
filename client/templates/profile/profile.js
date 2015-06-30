aad85963bc6d24b45679b217681ebc251b73be38
FrankyYang <rsail@126.com>
2015-06-30 14:07:58 +0800

    Little change for the profile page.

--- client/templates/profile/profile.js
+++ client/templates/profile/profile.js
@@ -3,7 +3,16 @@ Template.profile.rendered = function () {
 
 Template.profile.helpers({
     profile: function () {
-        return {};
+        var u = Meteor.user();
+        var obj = {
+            emailAddress: u? u.emails[0].address : '暂无邮箱信息',
+            phone: u ? u.phone : '暂无手机号',
+            photo: u ? u.photo : 'img/contacts/15.jpg',
+            website: u ? u.website : 'www.startingx.com',
+            weiboUrl: u ? u.weiboUrl : '暂无微博信息',
+            address: u ? u.address : '暂无住址信息',
+        };
+        return util._copyAndAddItem(Meteor.user(), obj);
     },
     members: function () {
         return [];
